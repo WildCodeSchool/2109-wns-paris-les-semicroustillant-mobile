927304cfa73374a6b8537ed007e5710b6c5cc9de
Fredy <fredy.ly.wcs@gmail.com>
2022-02-22 11:23:10 +0100

    refacto UserScreen

--- screens/UserScreen.tsx
+++ screens/UserScreen.tsx
@@ -2,29 +2,30 @@ import React, { useState } from 'react';
 import {
   StyleSheet,
   SafeAreaView,
-  Pressable,
   FlatList,
   ListRenderItem,
 } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 import { Card, SearchBar } from 'react-native-elements';
-import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import useColorScheme from '../hooks/useColorScheme';
-import AvatarComponent from '../components/AvatarComponent';
-interface IUser {
-  _id: number;
+import UserItemComponent from '../components/UserItemComponent';
+
+export interface IUser {
+  _id: string;
   firstname: string;
   lastname: string;
-  email: string;
-  hash: string;
-  role: string;
+  email?: string;
+  hash?: string;
+  role?: string;
   position: string;
+}
+
+export interface IUserItem extends IUser {
+  navigation?: {};
 }
 
 const fakeData: any = [
   {
-    _id: 1,
+    _id: '1',
     firstname: 'Bobby',
     lastname: 'Billy',
     email: 'bobi@email.com',
@@ -33,7 +34,7 @@ const fakeData: any = [
     position: 'Product Owner',
   },
   {
-    _id: 2,
+    _id: '2',
     firstname: 'Jane',
     lastname: 'Fixme',
     email: 'jfix@email.com',
@@ -42,7 +43,7 @@ const fakeData: any = [
     position: 'Developer',
   },
   {
-    _id: 3,
+    _id: '3',
     firstname: 'Joe',
     lastname: '',
     email: 'jdal@email.com',
@@ -51,7 +52,7 @@ const fakeData: any = [
     position: 'Product Owner',
   },
   {
-    _id: 4,
+    _id: '4',
     firstname: 'Pete',
     lastname: 'Do De La Vega',
     email: 'pd@email.com',
@@ -60,7 +61,7 @@ const fakeData: any = [
     position: 'Developer',
   },
   {
-    _id: 5,
+    _id: '5',
     firstname: 'Bubu',
     lastname: 'de More',
     email: 'bb@email.com',
@@ -73,56 +74,21 @@ const fakeData: any = [
 export default function UserScreen({
   navigation,
 }: RootTabScreenProps<'Users'>) {
-  const colorScheme = useColorScheme();
-
   const [search, setSearch] = useState('');
   const updateSearch = (search: string) => {
     setSearch(search);
   };
 
-  const UserItem = ({ user }: IUser) => {
+  const renderItem: ListRenderItem<IUserItem> = ({ item }) => {
     return (
-      <>
-        <View style={styles.userItem}>
-          <AvatarComponent
-            size={64}
-            position={user.position}
-            lastname={user.lastname}
-            firstname={user.firstname}
-          />
-          <View style={styles.innerItem}>
-            <View style={styles.userText}>
-              <Text style={styles.names}>
-                {user.firstname} {user.lastname}
-              </Text>
-              <Text style={styles.position}>{user.position}</Text>
-            </View>
-            <Pressable
-              onPress={() => navigation.navigate('UserEdit', user._id)}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}
-            >
-              <Ionicons
-                name="ios-chevron-forward"
-                size={25}
-                color={'#F50D51'}
-                style={{ fontSize: 40 }}
-              />
-            </Pressable>
-          </View>
-        </View>
-        <View
-          style={styles.separator}
-          lightColor="lightgrey"
-          darkColor="rgba(255,255,255,0.1)"
-        />
-      </>
+      <UserItemComponent
+        _id={item._id}
+        firstname={item.firstname}
+        lastname={item.lastname}
+        position={item.position}
+        navigation={navigation}
+      />
     );
-  };
-
-  const renderItem: ListRenderItem<IUser> = ({ item }) => {
-    return <UserItem user={item} />;
   };
 
   return (
@@ -160,37 +126,6 @@ const styles = StyleSheet.create({
     marginTop: 0,
     paddingTop: 0,
     paddingBottom: 0,
-  },
-  userItem: {
-    paddingTop: 20,
-    paddingBottom: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  innerItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flex: 1,
-  },
-  userText: {
-    flexWrap: 'nowrap',
-    flex: 1,
-  },
-  names: {
-    flex: 1,
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  position: {
-    flex: 1,
-    fontSize: 15,
-    color: 'grey',
-  },
-  separator: {
-    height: 1,
-    width: '97%',
-    alignSelf: 'center',
   },
   searchbarContainer: {
     backgroundColor: 'transparent',
